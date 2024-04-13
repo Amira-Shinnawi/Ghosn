858f524e21831b5bcd352d64a140b94185e04ed8
Amira-Shinnawi <amirashenawy123@gmail.com>
2024-04-13 03:54:25 +0200

    some edit

--- lib/User Features/home/presentation/views/home.dart
+++ lib/User Features/home/presentation/views/home.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import '../../../../constants.dart';
 import 'widgets/categoriesBar.dart';
 import 'widgets/search&filter.dart';
-import 'widgets/search&filter.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -11,37 +10,32 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: bgColor,
-      appBar: AppBar(
         backgroundColor: bgColor,
-        title: const Text(
-          "Hi , user",
-          style: TextStyle(color: Colors.black),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.shopping_cart_outlined),
+        appBar: AppBar(
+          backgroundColor: bgColor,
+          title: const Text(
+            "Hi , user",
+            style: TextStyle(color: Colors.black),
           ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.notifications_outlined),
-          )
-        ],
-      ),
-      body: Expanded(
-        child: Column(
-          children: [
-            Padding(padding: const EdgeInsets.all(8.0), child: searchPfilter()),
-            categoriesBar()
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.shopping_cart_outlined),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.notifications_outlined),
+            )
           ],
         ),
-      body: Column(
-        children: [
-          Padding(padding: const EdgeInsets.all(8.0), child: searchPfilter()),
-          categoriesBar()
-        ],
-      ),
-    );
+        body: Expanded(
+          child: Column(
+            children: [
+              Padding(
+                  padding: const EdgeInsets.all(8.0), child: searchPfilter()),
+              categoriesBar()
+            ],
+          ),
+        ));
   }
 }
